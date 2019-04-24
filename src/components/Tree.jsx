4e24c073436afe6e954a53f7c217a1a95f648551
marhovey <1063065755@qq.com>
2019-04-24 22:14:58 +0800

    init-tree

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -12,27 +12,29 @@ class Tree extends Component {
       config: {
         type: 'tree',
         parentId: 'pid',
-        id: 'id'
+        id: 'id',
+        value: 'value',
+        label: 'label'
       }
     }
   }
 
   componentWillMount () {
-    if (this.props.treeType.toLowerCase() === 'tree') {
+    if (this.props.config.type.toLowerCase() === 'tree') {
       this.setState({
         treeData: this.props.treeData,
-        type: this.props.treeType.toLowerCase()
+        config: this.props.config
       })
     } else {
       this.setState({
         treeArray: this.props.treeData,
-        type: this.props.treeType.toLowerCase()
+        config: this.props.config
       })
     }
   }
 
   componentDidMount () {
-    if (this.state.type !== 'tree') {
+    if (this.state.config.type.toLowerCase() !== 'tree') {
       this.factoryArrayData()
     } else {
       this.factoryTreeData()
@@ -48,18 +50,68 @@ class Tree extends Component {
   }
 
   factoryArrayData () {
-    let data = this.state.treeArray, obj;
-    data.map((v, i) => {})
+    let data = this.state.treeArray, obj = {}, rootId = null;
+    data.map((v, i) => {
+      if(v[this.state.config.parentId] || v[this.state.config.parentId] === 0) {
+          if (obj[v[this.state.config.parentId]]) {
+              if (obj[v[this.state.config.parentId]].children) {
+                  obj[v[this.state.config.parentId]].children.push(v)
+              } else {
+                  obj[v[this.state.config.parentId]].children = [v]
+              }
+          } else {
+              obj[v[this.state.config.parentId]] = {
+                  children: [v]
+              }
+          }
+      } else {
+          rootId = v[this.state.config.id]
+      }
+      if (obj[v[this.state.config.id]]) {
+        v.children = obj[v[this.state.config.id]].children
+      }
+      obj[v[this.state.config.id]] = v
+    })
+    this.setState({
+      treeData: obj[rootId],
+      treeObj: obj
+    })
   }
 
   factoryTreeData () {
     let data = this.state.treeData
   }
 
+  renderTreeParent () {
+    return (
+        <div className="parentNode">
+            {this.state.treeData[this.state.config.label]}
+            {
+              this.state.treeData.children?
+                  this.renderTreeNode(this.state.treeData.children):null
+            }
+        </div>
+    )
+  }
+
+  renderTreeNode (data) {
+    return data.map((val, ind) => {
+      return (
+          <div key={ind} className="childNode">
+              {val[this.state.config.label]}
+              {
+                val.children?
+                    this.renderTreeNode(val.children):null
+              }
+          </div>
+      )
+    })
+  }
+
   render () {
     return (
       <div className="tree">
-        哈哈哈
+          {this.renderTreeParent()}
       </div>
     )
   }
